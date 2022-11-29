f03a7145f32e60551608ac7d2d4a9bdcaaa61cc7
Maksym Kolii <maksymkolii@gmail.com>
2022-11-29 23:49:05 +0100

    commit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -12,10 +12,10 @@ import { Section } from './Section/Section';
 export class App extends Component {
   state = {
     contacts: [
-      // { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      // { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      // { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      // { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
     ],
     filter: '',
   };
@@ -33,7 +33,7 @@ export class App extends Component {
 
   //! НУ делаем Публичным св-вом -(стрелочной ФУ!)
   //* Делать только методом Класса !
-  componentDidUpdate(prevProps, prevState) {
+  componentDidUpdate(_, prevState) {
     console.log('App componentDidUpdate');
 
     if (this.state.contacts !== prevState.contacts) {
@@ -58,7 +58,7 @@ export class App extends Component {
     isNameExist
       ? window.alert(`${newContact.name} is alredy in contacts!`)
       : this.setState(prevState => ({
-          contacts: [...prevState.contacts, newContact],
+          contacts: [newContact, ...prevState.contacts],
         }));
   };
   deleteContact = iD => {
--- src/components/ContactItem/ContactItem.jsx
+++ src/components/ContactItem/ContactItem.jsx
@@ -1,5 +1,7 @@
+import { IconButton } from 'components/IconButtons/IconButton';
 import PropTypes from 'prop-types';
-import { Span, List, P, Button } from './ContactItem.styled';
+import { Span, List, P } from './ContactItem.styled';
+import { ReactComponent as DeleteIcon } from '../../Icons/delete2.svg';
 
 export const ContactItem = ({ id, name, number, onClickDelete }) => {
   return (
@@ -10,9 +12,13 @@ export const ContactItem = ({ id, name, number, onClickDelete }) => {
       <P>
         Number:<Span>{number}</Span>
       </P>
-      <Button className="" type="button" onClick={onClickDelete}>
+      {/* <Button className="" type="button" onClick={onClickDelete}>
         Delete
-      </Button>
+      </Button> */}
+
+      <IconButton onClick={onClickDelete} aria-label="Удалить контакт">
+        <DeleteIcon width={20} height={20}></DeleteIcon>
+      </IconButton>
     </List>
   );
 };
